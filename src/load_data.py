9886e2459315fb1cd5f30a9d282e632a990a78fd
Peteraya <up201505460@fe.up.pt>
2018-07-09 17:51:19 +0100

    Results reader done

--- src/load_data.py
+++ src/load_data.py
@@ -3,19 +3,59 @@ import glob
 import sys
 import os
 
-path =r'Results/Scenario-1/' # use your path
-allFiles = glob.glob(path + "/*.csv")
 
-frame = pd.DataFrame()
-list_ = []
-for index in range(len(allFiles)-1):
-   df = pd.read_csv(allFiles[index],index_col=None, header=0)
-   list_.append(df)
+""" 
+if os.path.isfile("../DataSet/results.csv"):
+	print("Existe")
 
-frame = pd.concat(list_)
+else:
+	print("nope")
+	"""
+
+def create_results(dataset_directory):
+    path =dataset_directory
+    results_list = []
+    results = pd.DataFrame()
+
+    for index_scn in range(10):
+    	new_path = path + str(index_scn+1)+'/'
+    	print(new_path)
+    	allFiles = glob.glob(new_path + "/*.csv")
+    	frame = pd.DataFrame()
+    	list_ = []
+
+    	for index in range(len(allFiles)-1):
+    		df = pd.read_csv(allFiles[index], index_col = False, header=0)
+    		list_.append(df)
+
+    	frame = pd.concat(list_)
+    	scenario = []
+    	list_ = []
+    	begin=0
+    	end=10
+
+    	for index in range(200):
+    		list_.append(frame[begin:end].describe().loc[['mean']])
+    		begin+=10
+    		end+=10
+
+    	scenario = pd.concat(list_)
+    	del scenario['nSimulationRun']
+    	scenario["Scenario"] = str(index_scn+1)
+    	results_list.append(scenario)
 
 
-print(frame)
+    results = pd.concat(results_list)
+    results.to_csv('../DataSet/results.csv', sep=',',index = False)
+    return None
 
-#single_result = pd.read_csv("Results/Scenario-1/Results-1-1.csv", sep=',')
-#print(single_result)
+
+
+    
+
+create_results('../DataSet/Results/Scenario-')
+results = pd.read_csv('../DataSet/results.csv', index_col = False, sep=',')
+
+print(results)
+
+
