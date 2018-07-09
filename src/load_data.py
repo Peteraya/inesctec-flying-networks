7ad23c83bd13a693e1f5e793973d6330467f6be8
francis-andrade <up201503481@fe.up.pt>
2018-07-09 15:50:50 +0100

    read scenarios done

--- src/load_data.py
+++ src/load_data.py
@@ -19,3 +19,19 @@ print(frame)
 
 #single_result = pd.read_csv("Results/Scenario-1/Results-1-1.csv", sep=',')
 #print(single_result)
+
+def read_scenarios(dataset_directory):
+    '''abs_path = os.path.abspath(os.path.dirname(__file__))
+    path = os.path.join(abs_path, "../DataSet")'''
+    scenarios = []
+    for i in range(1, 11):
+        scenario_directory = os.path.join(dataset_directory, "Scenario-"+str(i))
+        csv_file = os.path.join(scenario_directory, "Users-Topology-"+str(i)+".csv")
+        scenario_frame = pd.read_csv(csv_file)
+        for j in range(0, len(scenario_frame)):
+            scenario_frame.loc[j, 'x'] = int((scenario_frame.loc[j, 'x']-15)/30)
+            scenario_frame.loc[j, 'y'] = int((scenario_frame.loc[j, 'y']-15)/30)
+        scenarios.append(scenario_frame)
+    return scenarios
+
+DATASET_DIRECTORY = "/home/francisco/Desktop/Estagio_INESCTEC/INESCTEC-FLYINGNETWORKS/DataSet"
