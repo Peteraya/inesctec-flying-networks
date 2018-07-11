dc0d314801049a123ec818430089c5f7c3f2d831
francis-andrade <up201503481@fe.up.pt>
2018-07-11 14:22:59 +0100

    read from files now works everywhere

--- src/load_data.py
+++ src/load_data.py
@@ -3,58 +3,58 @@ import glob
 import sys
 import os
 import platform
-from utils import * 
+from utils import *
 
 
-def create_results(results_directory):
+
+def create_results(dataset_directory):
+    results_directory = os.path.join(dataset_directory, "Results/Scenario-")
     path =results_directory
     results_list = []
-    results = pd.DataFrame()
-
     for index_scn in range(10):
-    	new_path = path + str(index_scn+1)+'/'
-    	print(new_path)
-    	allFiles = glob.glob(new_path + "/Results-*.csv")
-    	frame = pd.DataFrame()
-    	list_ = []
+        new_path = path + str(index_scn+1)+'/'
+        print(new_path)
+        list_scenario = []
+        for index in range(200):
+            allFiles = glob.glob(new_path + "/Results-"+str(index + 1)+"-*.csv")
+            frame = pd.DataFrame()
+            list_ = []
+            for index in range(len(allFiles)):
+                df = pd.read_csv(allFiles[index], index_col = False, header=0)
+                list_.append(df)
 
-    	for index in range(len(allFiles)):
-    		df = pd.read_csv(allFiles[index], index_col = False, header=0)
-    		list_.append(df)
+            frame = pd.concat(list_)
+            list_scenario.append(frame[0:10].describe().loc[['mean']])
+        scenario = []
+        scenario = pd.concat(list_scenario)
+        del scenario['nSimulationRun']
+        scenario["Scenario"] = str(index_scn+1)
+        results_list.append(scenario)
 
-    	frame = pd.concat(list_)
-    	scenario = []
-    	list_ = []
-    	begin=0
-    	end=10
-
-    	for index in range(200):
-    		list_.append(frame[begin:end].describe().loc[['mean']])
-    		begin+=10
-    		end+=10
-
-    	scenario = pd.concat(list_)
-    	del scenario['nSimulationRun']
-    	scenario["Scenario"] = str(index_scn+1)
-    	results_list.append(scenario)
-
-
+    results = pd.DataFrame()
     results = pd.concat(results_list)
-    results.to_csv('../DataSet/results.csv', sep=',',index = False)
+    results.loc[:, 'fmap1CoordinatesX'] = (results.loc[:, 'fmap1CoordinatesX'] - 15) / 30
+    results.loc[:, 'fmap2CoordinatesX'] = (results.loc[:, 'fmap2CoordinatesX'] - 15) / 30
+    results.loc[:, 'fmap3CoordinatesX'] = (results.loc[:, 'fmap3CoordinatesX'] - 15) / 30
+    results.loc[:, 'fmap1CoordinatesY'] = (results.loc[:, 'fmap1CoordinatesY'] - 15) / 30
+    results.loc[:, 'fmap2CoordinatesY'] = (results.loc[:, 'fmap2CoordinatesY'] - 15) / 30
+    results.loc[:, 'fmap3CoordinatesY'] = (results.loc[:, 'fmap3CoordinatesY'] - 15) / 30
+
+    results_file = os.path.join(dataset_directory, "results.csv")
+    results.to_csv(results_file, sep=',',index = False)
     return None
 
 
 def read_results(dataset_directory):
-    results_directory = os.path.join(dataset_directory, "Results/Scenario-")
     results_file = os.path.join(dataset_directory, "results.csv")
     if not(os.path.isfile(results_file)):
-    	create_results(results_directory)
+        create_results(dataset_directory)
 
     results = pd.read_csv(results_file, index_col = False, sep=',')
 
     return results
 
-   
+
 #create_results('../DataSet/Results/Scenario-')
 
 def read_scenarios(dataset_directory):
@@ -73,9 +73,9 @@ def read_scenarios(dataset_directory):
 abs_path = os.path.abspath(os.path.dirname(__file__))
 
 if platform.system() == "Windows" :
-	DATASET_DIRECTORY =  "../DataSet" 
+    DATASET_DIRECTORY =  "../DataSet"
 else:
-	DATASET_DIRECTORY = os.path.join(abs_path, "../DataSet")	
+    DATASET_DIRECTORY = os.path.join(abs_path, "../DataSet")
 
 
 
