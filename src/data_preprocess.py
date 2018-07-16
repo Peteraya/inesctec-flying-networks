e6feb71592f7d33bfe4cb2bebeaa470c0d0ca797
francis-andrade <up201503481@fe.up.pt>
2018-07-16 13:55:47 +0100

    some tweaks

--- src/data_preprocess.py
+++ src/data_preprocess.py
@@ -71,11 +71,6 @@ def drones_matrix(results_line):
     matrix[drone3_x][drone3_y] = 1
     return matrix, quality_items
 
-#This function is INCOMPLETE DON'T USE IT
-def build_model_structure_complete(scenarios, results):
-    for index_results in range(len(results)):
-        scenario_id = int (index_results / SCENARIO_TOPOLOGIES_NO) + 1
-        scenario_matrix = datarate_matrix(scenarios[scenario_id]) 
 
 def build_model_structure_transformation(model_structure, model_prediction, list_scenarios, list_topologies,function, angle):
     index_model_structure = 0
@@ -101,35 +96,39 @@ def build_model_structure(scenarios, results, list_scenarios, list_topologies):
     index_scenario = 0
     for scenario_id in list_scenarios:
         scenario_begin_index = (scenario_id - 1) * SCENARIO_TOPOLOGIES_NO
-        scenario_matrix = normalize_matrix(datarate_matrix(scenarios[scenario_id - 1]))
+        scenario_matrix = datarate_matrix(scenarios[scenario_id - 1])
+        if(NORMALIZE_DATA == 1):
+            scenario_matrix = normalize_matrix(scenario_matrix)
         for topology_id in list_topologies[index_scenario]:
             index_results = scenario_begin_index + topology_id - 1
-            topology_matrix_aux, qualities_list = drones_matrix(results.loc[index_results])
-            if(DISTANCE_ENCODING == 0):
-                topology_matrix = normalize_sparse_topology(topology_matrix_aux)
-            else:
-                topology_matrix = normalize_matrix(sparse_to_distance(topology_matrix_aux))
+            topology_matrix, qualities_list = drones_matrix(results.loc[index_results])
+            if(DISTANCE_ENCODING == 1):
+                topology_matrix = sparse_to_distance(topology_matrix)
+            if(NORMALIZE_DATA == 1):
+                topology_matrix = normalize_matrix(topology_matrix)
             model_struct_orig.append([scenario_matrix, topology_matrix])
             model_prediction.append(qualities_list)
         index_scenario += 1
-    #Then the topologies with a 90 rotation
-    model_struct_rot1, model_pred_rot1 = build_model_structure_transformation(model_struct_orig, model_prediction, list_scenarios, list_topologies, rotate, 90)
-    #Then the topologies with a 180 rotation
-    model_struct_rot2, model_pred_rot2 = build_model_structure_transformation(model_struct_orig, model_prediction, list_scenarios, list_topologies, rotate, 180)
-    #Then the topologies with a 270 rotation
-    model_struct_rot3, model_pred_rot3 = build_model_structure_transformation(model_struct_orig, model_prediction, list_scenarios, list_topologies, rotate, 270)
-    #Then the topologies with a symmetry over the 0 axis
-    model_struct_sym1, model_pred_sym1 = build_model_structure_transformation(model_struct_orig, model_prediction, list_scenarios, list_topologies, symmetric, 0)
-    #Then the topologies with a symmetry over the 45 axis
-    model_struct_sym2, model_pred_sym2 = build_model_structure_transformation(model_struct_orig, model_prediction, list_scenarios, list_topologies, symmetric, 45)
-    #Then the topologies with a symmetry over the 90 axis
-    model_struct_sym3, model_pred_sym3 = build_model_structure_transformation(model_struct_orig, model_prediction, list_scenarios, list_topologies, symmetric, 90)
-    #Then the topologies with a symmetry over the 135 axis
-    model_struct_sym4, model_pred_sym4 = build_model_structure_transformation(model_struct_orig, model_prediction, list_scenarios, list_topologies, symmetric, 135)
+    if(USE_TRANSFORMATIONS == 1):
+        #Then the topologies with a 90 rotation
+        model_struct_rot1, model_pred_rot1 = build_model_structure_transformation(model_struct_orig, model_prediction, list_scenarios, list_topologies, rotate, 90)
+        #Then the topologies with a 180 rotation
+        model_struct_rot2, model_pred_rot2 = build_model_structure_transformation(model_struct_orig, model_prediction, list_scenarios, list_topologies, rotate, 180)
+        #Then the topologies with a 270 rotation
+        model_struct_rot3, model_pred_rot3 = build_model_structure_transformation(model_struct_orig, model_prediction, list_scenarios, list_topologies, rotate, 270)
+        #Then the topologies with a symmetry over the 0 axis
+        model_struct_sym1, model_pred_sym1 = build_model_structure_transformation(model_struct_orig, model_prediction, list_scenarios, list_topologies, symmetric, 0)
+        #Then the topologies with a symmetry over the 45 axis
+        model_struct_sym2, model_pred_sym2 = build_model_structure_transformation(model_struct_orig, model_prediction, list_scenarios, list_topologies, symmetric, 45)
+        #Then the topologies with a symmetry over the 90 axis
+        model_struct_sym3, model_pred_sym3 = build_model_structure_transformation(model_struct_orig, model_prediction, list_scenarios, list_topologies, symmetric, 90)
+        #Then the topologies with a symmetry over the 135 axis
+        model_struct_sym4, model_pred_sym4 = build_model_structure_transformation(model_struct_orig, model_prediction, list_scenarios, list_topologies, symmetric, 135)
 
-    return (model_struct_orig + model_struct_rot1 + model_struct_rot2 + model_struct_rot3 + model_struct_sym1 + model_struct_sym2 + model_struct_sym3 + model_struct_sym4),(model_prediction + model_pred_rot1 + model_pred_rot2 + model_pred_rot3 + model_pred_sym1 + model_pred_sym2 + model_pred_sym3 + model_pred_sym4)
-            
-
+        return (model_struct_orig + model_struct_rot1 + model_struct_rot2 + model_struct_rot3 + model_struct_sym1 + model_struct_sym2 + model_struct_sym3 + model_struct_sym4),(model_prediction + model_pred_rot1 + model_pred_rot2 + model_pred_rot3 + model_pred_sym1 + model_pred_sym2 + model_pred_sym3 + model_pred_sym4)
+    else:
+        return model_struct_orig, model_prediction      
+#Decided to not use this function in order to make the code more generic
 def normalize_sparse_topology(matrix):
     new_matrix = np.empty((len(matrix), len(matrix[0])), dtype = "float")
     mean = (97*0+3*1)/100
